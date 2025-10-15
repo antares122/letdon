d8a2d766519d612e58fe670bad951fbc660aee7e
antares122 <balikgj9@gmail.com>
2025-10-15 11:15:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -19,3 +19,15 @@ playBtn.addEventListener("click", function() {
     wave.classList.remove("active");
   }
 });
+  const video = document.getElementById("bgVideo");
+  const btn = document.getElementById("playPauseBtn");
+
+  btn.addEventListener("click", () => {
+    if (video.paused) {
+      video.play();
+      btn.textContent = "⏸ Pause";
+    } else {
+      video.pause();
+      btn.textContent = "▶ Play";
+    }
+  });
